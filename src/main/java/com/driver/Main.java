77bf613e79eca030b6a06ac4602f4f7927c212e7
Puspanjali <puspanjalipriyadarshini9@email.com>
2022-12-16 13:28:35 +0530

    Code

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args){
+        RWOnly obj = new RWOnly();
+        obj.SetName("veera Phani");
+        System.out.println(obj.getName());
+    }
   
 }
